7bc8ff6edbc740ef560b45f97cafc3060a1c3175
Matt Gedigian <mgedigian@gmail.com>
2009-03-05 03:15:14 -0800

    Added command line arguments to query.py

--- query.py
+++ query.py
@@ -2,67 +2,105 @@ import os
 import sys
 import config_manager as config
 import client as mwclient
-import TableParse    
+import getopt
+
+verbose = False
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
 
 class Query(object):
+    global verbose
     def __init__(self, query, opts=None, site=None):
-        global verbose
-        verbose=False
         if not site:
-            print "Connecting as %s to http://%s%s" % (config.get("username"), config.get("site"), config.get("path"))
+            print >> sys.stderr, "Connecting as %s to http://%s%s" % (config.get("username"), config.get("site"), config.get("path"))
             self.site  = mwclient.Site(config.get("site"), path=config.get("path"))
             self.site.login(config.get("username"),config.get("password"))
         else:
             self.site = site    
+        self.opts = opts;
         if opts:
             query_parts = query.split("}}")
-            query = query_parts[0] + " | " + " | ".join(k + "=" + str(v) for k,v in opts.items()) + "}}" + query_parts[1]
+            self.query = query_parts[0] + " | " + " | ".join(k + "=" + str(v) for k,v in opts.items()) + "}}" + query_parts[1]
 
+    def execute(self):
         if verbose:
-            print "Parsing query:", query
-        results = self.site.parse(query)
-        if opts["format"].lower() == "csv":
-            csv  = results['text']['*']
-            print "NO CSV PROCESSING?"
-            return
+            print "Parsing query:", self.query
+        results = self.site.parse(self.query)
+        if self.opts["format"].lower() == "csv":
+            result = results
         else:
-            html = results['text']['*']
+            result = results['text']['*']
             if verbose:
-                print "Got html", html
-            self.lol = TableParse.parse(html)
-        if verbose:
-            print "Parsed return", str(type(self.lol)), 'of length', len(self.lol)
+                print "Got html", result.encode("utf-8")
+        return result
 
     def result_lol(self):
         return self.lol
         
-    def result_dict(self):
-        result_dict = {}
-        if len(self.lol):
-            for row in self.lol:
-                if len(row) == 0: continue
-                try:
-                    key = row[0]
-                    val = row[1]
-                    if len(key) > 0:
-                        result_dict[key] = val
-                except:
-                    print "Bad Row", row
-        return result_dict
-    
 
-if __name__ == '__main__':
-    global verbose
-    verbose = True
-    opts = {"limit":500, "format":"html"}
-    if len(sys.argv) == 1:
-        query = '{{#ask: [[Category::Disease]] [[Summary::+]] | ?Summary }}'
-    elif len(sys.argv) == 2:
-        query = sys.argv[1]
-    else:
-        print "Only take one argument"
-    q = Query(query, opts)
-    summary_dict = q.result_dict()
-    for article,summary in summary_dict.items():
-        print article + ":\n  " + summary + "\n"
+def html2lol(html):
+    import TableParse    
+    lol = TableParse.parse(html)
+    if verbose:
+        print "Parsed return", str(type(lol)), 'of length', len(lol)
+    for l in lol:
+        print "\t\t".join(l) + "\n"
     
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    global verbose
+    limit = 500
+    format = "html"
+    reformat_html = False
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "hl:f:vr", ["help", "limit=", "format=", "verbose", "reformat"])
+        except getopt.error, msg:
+            raise Usage(msg)
+
+        # option processing
+        for option, value in opts:
+            if option in ("-v", "--verbose"):
+                verbose = True
+            elif option in ("-h", "--help"):
+                raise Usage(help_message)
+            elif option in ("-l", "--limit"):
+                limit = int(value)
+            elif option in ("-f", "--format"):
+                format = value
+            elif option in ("-r", "--reformat"):
+                reformat_html = True
+            else:
+                print "What is this:", option
+                  
+        if reformat_html and not format == "html":
+            raise Usage("We can only reformat HTML")
+        if len(args) == 0:
+            query = '{{#ask: [[Category::Disease]] [[Summary::+]] | ?Summary }}'
+        elif len(args) == 1:
+            query = args[0]
+        else:
+            raise Usage("We only can handle a single query argument")
+
+        opts = {"limit":limit, "format":format}
+        q = Query(query, opts)
+        result = q.execute();
+        if reformat_html:
+            html2lol(result)
+        else:
+            print result.encode("utf-8")
+        # summary_dict = q.result_dict()
+        # for article,summary in summary_dict.items():
+        #     print article + ":\n  " + summary + "\n"
+    except Usage, err:
+        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+        print >> sys.stderr, "\t for help use --help"
+        return 2
+
+
+if __name__ == "__main__":
+    sys.exit(main())
